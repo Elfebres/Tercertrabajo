8f238b97439e4895eb0a447cac5e7a59d19a6188
Elfebres <dragiti1@gmail.com>
2021-06-27 02:10:31 -0500

    YEs

---
+++ script.js
@@ -0,0 +1,5 @@
+// *******************************
+// COMIENCE AQUÍ SI QUIERE UN PUNTO DE INICIO MÁS FÁCIL PARA ESTA ASIGNACIÓN
+// *******************************
+//
+// Instrucciones de asignación del módulo 4.
